8b26f71f20e24d84a70ce2a6d164987894578760
recyx <fe.schulz@gmx.net>
2018-04-21 10:09:43 +0200

    did stfu

---
+++ Scripts/Turret.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	public Transform platform;
+
+	public float range;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = platform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
